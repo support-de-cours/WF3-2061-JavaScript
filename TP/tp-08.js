ddd5c1ad6850111c09f7c48495f2397e75e056a3
Arnaud BODEL <contact@osw3.net>
2023-03-20 15:25:41 +0100

    -

--- TP/tp-08.js
+++ TP/tp-08.js
@@ -3,3 +3,18 @@
 // -> uns balise <div id="counter">
 // -> Boutton <button xxxx>Click Me</button>
 
+const btn = document.querySelector('#btn');
+const counter = document.querySelector('#counter');
+let countClick = 0;
+
+function showCounter()
+{
+    counter.innerHTML = countClick;
+}
+
+btn.addEventListener('click', () => {
+    countClick++;
+    showCounter();
+});
+
+showCounter();
