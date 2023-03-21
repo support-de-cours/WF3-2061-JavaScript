797c656e9f5d909e8adadc6bad3aa05cacab825a
Arnaud BODEL <contact@osw3.net>
2023-03-21 16:04:15 +0100

    Ajout du formulaire

--- 13-formulaire/script.js
+++ 13-formulaire/script.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// Indiquer les champs obligatoires
+// --
+
 // 1. Cibler tous les éléments qui possède l'attribut "required"
 // 2. remoter sur son élément parent
 // 3. Cibler l'élément frere "label"
@@ -38,4 +41,65 @@ for (const item of arr_required)
 
     // Etape 4
     label.classList.add('required');
+}
+
+
+
+// Générer les listes de selections du champ "Birthday"
+// --
+
+const months = [
+    "janvier",
+    "février",
+    "mars",
+    "avril",
+    "mai",
+    "juin",
+    "juillet",
+    "aout",
+    "septembre",
+    "octobre",
+    "novembre",
+    "décembre"
+];
+
+// Liste des jours
+const select_day = document.querySelector('[name="birthday[day]"]');
+
+for (let i=1; i<=31; i++)
+{
+    let el_option = document.createElement('option');
+        el_option.value = i;
+        el_option.innerText = i<=9 ? `0${i}` : i;
+
+    select_day.append( el_option );
+}
+
+
+// Liste des mois
+const select_month = document.querySelector('[name="birthday[month]"]');
+
+for (let i=0; i<months.length; i++)
+{
+    let el_option = document.createElement('option');
+        el_option.value = (i+1);
+        el_option.innerText = months[i];
+
+    select_month.append( el_option );
+}
+
+
+// Liste des années
+const select_year = document.querySelector('[name="birthday[year]"]');
+
+const date = new Date();
+const year = date.getFullYear();
+const minYear = year-100;
+
+for (let i=year; i>=minYear; i--)
+{
+    let el_option = document.createElement('option');
+        el_option.innerText = i;
+
+    select_year.append( el_option );
 }
