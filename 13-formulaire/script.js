29876d4294186178639fe6f9349291535f92b81f
Arnaud BODEL <contact@osw3.net>
2023-03-21 15:02:58 +0100

    Ajout du formulaire

--- 13-formulaire/script.js
+++ 13-formulaire/script.js
@@ -1,2 +1,41 @@
 "use strict";
 
+// 1. Cibler tous les éléments qui possède l'attribut "required"
+// 2. remoter sur son élément parent
+// 3. Cibler l'élément frere "label"
+// 4. Injecter la class "required" sur l'element "label"
+
+// Etape 1
+// --
+const arr_required = document.querySelectorAll('[required]');
+
+// Etape 2
+// --
+for (const item of arr_required)
+{
+    let parent;
+    let label;
+
+    // Etape 3.a
+    if (item.nodeName === 'INPUT')
+    {
+        parent = item.parentNode;
+    }
+    else if (item.nodeName === 'SELECT')
+    {
+        parent = item.parentNode.parentNode.parentNode;
+    }
+
+    // Etape 3.b
+    if (parent.nodeName === 'DIV')
+    {
+        label = parent.querySelector('label');
+    }
+    else
+    {
+        label = parent;
+    }
+
+    // Etape 4
+    label.classList.add('required');
+}
