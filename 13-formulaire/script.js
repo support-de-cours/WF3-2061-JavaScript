31b03e9add02bf48c151eac0905bd7d89668f13e
Arnaud BODEL <contact@osw3.net>
2023-03-22 14:19:39 +0100

    Ajout du formulaire

--- 13-formulaire/script.js
+++ 13-formulaire/script.js
@@ -119,6 +119,10 @@ for (let i=year; i>=minYear; i--)
 const form = document.querySelector('form');
 const el_firstname = document.querySelector('input[name=firstname]');
 const el_lastname = document.querySelector('input[name=lastname]');
+const el_email = document.querySelector('input[name=email]');
+const el_password = document.querySelector('input[name=password]');
+const el_confirm_password = document.querySelector('input[name=confirmpwd]');
+const el_agreeTerms = document.querySelector('input[name=agreeTerms]');
 
 
 /**
@@ -145,6 +149,7 @@ function showError(target, message)
  */
 function removeError()
 {
+    console.log('REMOVE IS-INVALID');
     const isInvalid = document.querySelectorAll('.is-invalid');
 
     for (const item of isInvalid)
@@ -153,6 +158,7 @@ function removeError()
     }
 
 
+    console.log('REMOVE ERROR');
     const errors = document.querySelectorAll('.error');
 
     for (const item of errors)
@@ -187,6 +193,145 @@ function checkFirstname()
     return error;
 }
 
+/**
+ * Check the lastname constraints
+ * 
+ * @returns {bool} true if firstname is valid
+ */
+function checkLastname()
+{
+    // Check lastname
+    let lastname = el_lastname.value;
+    let error = false;
+    // let re = /^[a-z][a-z-\s]*[a-z]?$/i;
+    let re = new RegExp(/^[a-z][a-z-\s]*[a-z]?$/, "i");
+
+    // lastname is required
+    if (!lastname.length)
+    {
+        showError(el_lastname, "Le champ lastname est obligatoire !!");
+        error = true;
+    }
+    else if (!re.test(lastname))
+    {
+        showError(el_lastname, "Le Nom doit contenir uniquement des caractères alphabétique.");
+        error = true;
+    }
+
+    return error;
+}
+
+function checkEmail()
+{
+    let email = el_email.value;
+    let error = false;
+    let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (!email.length)
+    {
+        showError(el_email, "Le champ email est obligatoire !!");
+        error = true;
+    }
+    else if (!re.test(email))
+    {
+        showError(el_email, "L'adresse email n'est pas valide !");
+        error = true;
+    }
+
+    return error;
+}
+
+function checkPassword()
+{
+    let password = el_password.value;
+    let error = false;
+    let minChar = 8;
+    let maxChar = 16;
+    let specialChar = "+=!-_§";
+    let re_specialChar = new RegExp(`/[${specialChar}]/`, "g");
+
+    // - required
+    if (!password.length)
+    {
+        showError(el_password, "Le champ password est obligatoire !!");
+        error = true;
+    }
+
+    // - longueur min 8 caractères
+    else if (password.length < minChar)
+    {
+        showError(el_password, `Le champ password doit contenir minimum ${minChar} caractères !!`);
+        error = true;
+    }
+
+    // - longueur max 16 caractères
+    else if (password.length > maxChar)
+    {
+        showError(el_password, `Le champ password doit contenir maximum ${maxChar} caractères !!`);
+        error = true;
+    }
+
+    // - au moins un caractère alphabetique
+    else if (!/[a-z]/.test(password))
+    {
+        showError(el_password, `Le champ password doit contenir au moins un caractère alphabétique !!`);
+        error = true;
+    }
+
+    // - au moins un caractère Majuscule
+    else if (!/[A-Z]/.test(password))
+    {
+        showError(el_password, `Le champ password doit contenir au moins un caractère alphabétique majuscule !!`);
+        error = true;
+    }
+
+    // - au moins un caractère numerique
+    else if (!/[0-9]/.test(password))
+    {
+        showError(el_password, `Le champ password doit contenir au moins un caractère numérique !!`);
+        error = true;
+    }
+
+    // - au moins un caractère spécial (+=.?!-_§&@#)
+    // else if (!re_specialChar.test(password))
+    // {
+    //     showError(el_password, `Le champ password doit contenir au moins un caractère spécial (${specialChar}) !!`);
+    //     error = true;
+    // }
+
+
+    return error;
+}
+
+function confirmPassword()
+{
+    let password = el_password.value;
+    let confirmation = el_confirm_password.value;
+    let error = false;
+
+    if (confirmation !== password)
+    {
+        showError(el_confirm_password, `Les mots de passe doivent être identique !`);
+        error = true;
+    }
+
+    return error;
+}
+
+function checkTerms()
+{
+    let agreeTerms = el_agreeTerms.checked;
+    let error = false;
+
+    if (!agreeTerms)
+    {
+        showError(el_agreeTerms, `Vous devez accepter les CGU !`);
+        error = true;
+    }
+    
+    return false;
+}
+
 
 
 form.addEventListener('submit', event => {
@@ -203,27 +348,28 @@ form.addEventListener('submit', event => {
     }
     
     // Check Lastname
-    // - required
-    // - alpha chars and - only
-
+    if (checkLastname())
+    {
+        error = true;
+    }
 
     // Check Email
-    // - required
-    // - user@server.com
-
+    if (checkEmail())
+    {
+        error = true;
+    }
 
     // Check Password
-    // - required
-    // - longueur min 8 caractères
-    // - longueur max 16 caractères
-    // - au moins un caractère alphabetique
-    // - au moins un caractère Majuscule
-    // - au moins un caractère numerique
-    // - au moins un caractère spécial (+=.?!-_§&@#)
-
+    if (checkPassword())
+    {
+        error = true;
+    }
 
     // Confirm Password
-    // - doit etre identique au "password"
+    if (confirmPassword())
+    {
+        error = true;
+    }
 
 
     // Check Birthday
@@ -232,7 +378,10 @@ form.addEventListener('submit', event => {
 
 
     // Check Terms
-    // - la case doit etre cochée
+    if (checkTerms())
+    {
+        error = true;
+    }
 
 
 
@@ -242,5 +391,9 @@ form.addEventListener('submit', event => {
         event.preventDefault();
     }
 });
-el_firstname.addEventListener('blur', checkFirstname);
 
+el_firstname.addEventListener('blur', checkFirstname);
+el_lastname.addEventListener('blur', checkLastname);
+el_email.addEventListener('blur', checkEmail);
+el_password.addEventListener('blur', checkPassword);
+el_confirm_password.addEventListener('blur', confirmPassword);
